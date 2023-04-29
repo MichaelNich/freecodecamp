ec7fd8094f66afad4916a5ac5fdcd117eb0646aa
Nicolas M. Feliz <nicolas.2221209@aluno.faculdadedescomplica.com.br>
2023-04-29 05:56:25 -0300

    Update number_guess.sh

--- build-a-number-guessing-game-project/number_guess.sh
+++ build-a-number-guessing-game-project/number_guess.sh
@@ -1,10 +1,25 @@
-
 #!/bin/bash
-
-echo Enter your username:
-read name
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+#COLUMNS DB
+#USERNAME, GAMES_PLAYED, BEST_GAME
 
 GUESS_NUMBER=$((1 + RANDOM 1000 ))
 
-#COLUMNS DB
-#USERNAME, GAMES_PLAYED, BEST_GAME
+MAIN(){
+  echo Enter your username:
+  read name
+  PLAYER_NAME=$($PSQL="SELECT username")
+}
+
+GET_INFO_FROM_DATABASE(){
+
+}
+
+PLAY(){
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+  else
+    echo That is not an integer, guess again:
+  fi
+}
+MAIN

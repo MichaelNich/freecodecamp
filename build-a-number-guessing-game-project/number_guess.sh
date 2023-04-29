4e4ca0575807618fde652978c6bac0d5d5abc59b
Nicolas M. Feliz <nicolas.2221209@aluno.faculdadedescomplica.com.br>
2023-04-29 06:10:11 -0300

    Update number_guess.sh

--- build-a-number-guessing-game-project/number_guess.sh
+++ build-a-number-guessing-game-project/number_guess.sh
@@ -3,21 +3,31 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 #COLUMNS DB
 #USERNAME, GAMES_PLAYED, BEST_GAME
 
-GUESS_NUMBER=$((1 + RANDOM 1000 ))
+GUESS_NUMBER=$((1 + RANDOM % 1000 ))
 
 MAIN(){
   echo Enter your username:
   read name
-  PLAYER_NAME=$($PSQL="SELECT username")
+  PLAYER_DETAILS=$($PSQL "SELECT username, games_played, best_game FROM player WHERE username='$name'")
+  #check if player is new
+  if [[ -z $PLAYER_DETAILS ]]
+  then
+    echo Welcome, $name! It looks like this is your first time here.
+  else
+      IFS='|'
+      echo "$PLAYER_DETAILS" | while read USERNAME GAMES_PLAYED, BEST_GAME
+      do
+        echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+      done
+  fi
 }
 
-GET_INFO_FROM_DATABASE(){
-
-}
+#GET_INFO_FROM_DATABASE(){}
 
 PLAY(){
   if [[ $1 =~ ^[0-9]+$ ]]
   then
+  echo PPPP
   else
     echo That is not an integer, guess again:
   fi
